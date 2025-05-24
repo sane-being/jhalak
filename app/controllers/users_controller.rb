8a08548a672e12156dfa39e97023c67a06d769d2
shrey <117995509+sane-being@users.noreply.github.com>
2025-05-24 11:41:02 +0000

    user is authorized only to update his credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update ] # destroy ]
+  before_action :authorize_user, only: %i[ edit update ]
 
   # GET /users or /users.json
   def index
@@ -61,6 +62,11 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params.expect(:id))
+    end
+
+    # Check user authorization
+    def authorize_user
+      redirect_to root_path, alert: "You are not authorized to access this page" unless @user == current_user
     end
 
     # Only allow a list of trusted parameters through.
