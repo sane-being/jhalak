18174db80b74fd8e813d86ef7073ca666c1d32b6
shrey <117995509+sane-being@users.noreply.github.com>
2025-05-25 11:23:09 +0000

    user can accept follow request
    - code improvement & remove files that were not required
    - add authorization check for follow request in its controller
    - add buttons at appropriate places
    - buttons change as per request status
    - add follow requests link to header
    - index page of follow requests shows pending follow requests and followers (accepted requests)

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,62 +1,46 @@
 class FollowRequestsController < ApplicationController
-  before_action :set_follow_request, only: %i[ show edit update destroy ]
+  before_action :set_follow_request, only: %i[ update destroy ]
 
   # GET /follow_requests or /follow_requests.json
   def index
-    @follow_requests = FollowRequest.where(user_id: current_user.id).all
-  end
-
-  # GET /follow_requests/1 or /follow_requests/1.json
-  def show
-  end
-
-  # GET /follow_requests/new
-  def new
-    @follow_request = FollowRequest.new
-  end
-
-  # GET /follow_requests/1/edit
-  def edit
+    @follow_requests = FollowRequest.where(user_id: current_user.id, accepted: false).all
+    @accepted_follow_requests = FollowRequest.where(user_id: current_user.id, accepted: true).all
   end
 
   # POST /follow_requests or /follow_requests.json
   def create
     @follow_request = FollowRequest.new(follow_request_params)
     @follow_request.follower_id = current_user.id
-
-    respond_to do |format|
-      if @follow_request.save
-        format.html { redirect_to @follow_request.user, notice: "Follow request was successfully created." }
-        format.json { render :show, status: :created, location: @follow_request }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @follow_request.errors, status: :unprocessable_entity }
-      end
+    if @follow_request.save
+      redirect_back(fallback_location: root_path, notice: "Follow request was successfully sent.")
+    else
+      redirect_back(fallback_location: root_path, alert: "Follow request could not be sent.")
     end
   end
 
   # PATCH/PUT /follow_requests/1 or /follow_requests/1.json
   def update
-    respond_to do |format|
-      if @follow_request.update(follow_request_params)
-        format.html { redirect_to @follow_request, notice: "Follow request was successfully updated." }
-        format.json { render :show, status: :ok, location: @follow_request }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @follow_request.errors, status: :unprocessable_entity }
-      end
+    # Check user authorization
+    unless @follow_request.user == current_user
+      redirect_to root_path, alert: "You are not authorized to update/destroy this follow request"
+    end
+    
+    if @follow_request.update(follow_request_params)
+      redirect_back(fallback_location: root_path, notice: "Follow request was accepted.")
+    else
+      redirect_back(fallback_location: root_path, alert: "Follow request could not be accepted.")
     end
   end
 
   # DELETE /follow_requests/1 or /follow_requests/1.json
   def destroy
-    @user = @follow_request.user
-    @follow_request.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to @user, status: :see_other, notice: "Follow request was successfully destroyed." }
-      format.json { head :no_content }
+    # Check user authorization
+    unless [@follow_request.follower, @follow_request.user].include? current_user
+      redirect_to root_path, alert: "You are not authorized to update/destroy this follow request"
     end
+    
+    @follow_request.destroy!
+    redirect_back(fallback_location: root_path, notice: "Follow request was successfully deleted.")
   end
 
   private
@@ -65,14 +49,8 @@ class FollowRequestsController < ApplicationController
       @follow_request = FollowRequest.find(params.expect(:id))
     end
 
-    # Check user authorization: follower user can only #create or #destroy request
-    # followed user can only #edit, #update the request response to accept  
-    def authorize_user
-      redirect_to root_path, alert: "You are not authorized to access this page" unless @user == current_user
-    end
-
     # Only allow a list of trusted parameters through.
     def follow_request_params
-      params.expect(follow_request: [ :accepted, :user_id ])
+      params.expect(follow_request: [ :user_id ])
     end
 end
