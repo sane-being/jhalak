26aa83cc56e24fd1ecaf2c2517b01928af86e7ba
shrey <117995509+sane-being@users.noreply.github.com>
2025-05-25 13:12:52 +0000

    fix: user was not able to accept follow requests

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -9,7 +9,7 @@ class FollowRequestsController < ApplicationController
 
   # POST /follow_requests or /follow_requests.json
   def create
-    @follow_request = FollowRequest.new(follow_request_params)
+    @follow_request = FollowRequest.new(params.expect(follow_request: [ :user_id ]))
     @follow_request.follower_id = current_user.id
     if @follow_request.save
       redirect_back(fallback_location: root_path, notice: "Follow request was successfully sent.")
@@ -25,7 +25,7 @@ class FollowRequestsController < ApplicationController
       redirect_to root_path, alert: "You are not authorized to update/destroy this follow request"
     end
     
-    if @follow_request.update(follow_request_params)
+    if @follow_request.update(params.expect(follow_request: [ :accepted ]))
       redirect_back(fallback_location: root_path, notice: "Follow request was accepted.")
     else
       redirect_back(fallback_location: root_path, alert: "Follow request could not be accepted.")
@@ -47,10 +47,5 @@ class FollowRequestsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_follow_request
       @follow_request = FollowRequest.find(params.expect(:id))
-    end
-
-    # Only allow a list of trusted parameters through.
-    def follow_request_params
-      params.expect(follow_request: [ :user_id ])
     end
 end
